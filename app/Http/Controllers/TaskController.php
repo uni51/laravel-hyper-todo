24cc4222595619273bef2a2844c7e5e0a52de2f9
uni51 <uni51@users.noreply.github.com>
2019-07-23 13:59:20 +0900

    Chapter7. ToDoアプリのタスク編集機能を作る

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
-use App\Task;
 use App\Http\Requests\CreateTask;
+use App\Http\Requests\EditTask;
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -50,5 +51,34 @@ class TaskController extends Controller
         return redirect()->route('tasks.index', [
             'id' => $current_folder->id,
         ]);
-    }    
+    }
+    
+    /**
+     * GET /folders/{id}/tasks/{task_id}/edit
+     */
+    public function showEditForm(int $id, int $task_id)
+    {
+        $task = Task::find($task_id);
+
+        return view('tasks/edit', [
+            'task' => $task,
+        ]);
+    }
+    
+    public function edit(int $id, int $task_id, EditTask $request)
+    {
+        // 1
+        $task = Task::find($task_id);
+    
+        // 2
+        $task->title = $request->title;
+        $task->status = $request->status;
+        $task->due_date = $request->due_date;
+        $task->save();
+    
+        // 3
+        return redirect()->route('tasks.index', [
+            'id' => $task->folder_id,
+        ]);
+    }   
 }
